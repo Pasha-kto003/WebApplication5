db8bd4085101445998188e7fcc0e29cf0ee95c28
Pasha-kto <lukovnikov.pasha@gmail.com>
2021-10-18 12:53:53 +1000

    Добавьте файлы проекта.

---
+++ WebApplication5/Controllers/HomeController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private ApplicationContext db;
+        public HomeController(ILogger<HomeController> logger, ApplicationContext context)
+        {
+            _logger = logger;
+            db = context;
+        }
+        [Authorize(Roles = "admin, user")]
+        public async Task<IActionResult> Index()
+        {
+            string role = User.FindFirst(x => x.Type == ClaimsIdentity.DefaultRoleClaimType).Value;
+            ViewData["content"] = $"ваша роль {role}";
+            return View(await db.Phones.ToListAsync());
+
+        }
+        [Authorize(Roles = "admin")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id != null)
+            {
+                Phone phone = await db.Phones.FirstOrDefaultAsync(p => p.ID == id);
+                if (phone != null)
+                    return View(phone);
+            }
+            return NotFound();
+        }
+
+        [HttpGet]
+        [ActionName("Delete"), Authorize(Roles = "admin")]
+        public async Task<IActionResult> ConfirmDelete(int? id)
+        {
+            if (id != null)
+            {
+                Phone phone = await db.Phones.FirstOrDefaultAsync(p => p.ID == id);
+                if (phone != null)
+                    return View(phone);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        [ActionName("Delete"), Authorize(Roles = "admin")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id != null)
+            {
+                Phone phone = new Phone { ID = id.Value };
+                db.Entry(phone).State = EntityState.Deleted;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return NotFound();
+        }
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id != null)
+            {
+                Phone phone = await db.Phones.FirstOrDefaultAsync(p => p.ID == id);
+                if (phone != null)
+                    return View(phone);
+            }
+            return NotFound();
+        }
+        [HttpPost, ActionName("Edit"), Authorize(Roles = "admin")]
+        public async Task<IActionResult> Edit(Phone phone)
+        {
+            db.Phones.Update(phone);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> Create(Phone phone)
+        {
+            db.Phones.Add(phone);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        [Authorize(Roles = "admin, user")]
+        public IActionResult About()
+        {
+            return Content("Вход только для администратора");
+        }
+    }
+}
---
+++ WebApplication5/Models/Phone.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication5.Models
+{
+    public class Phone
+    {
+        public int ID { get; set; }
+        public string Model { get; set; }
+        public string Company { get; set; }
+        public decimal Price { get; set; }
+        public string OS { get; set; }
+        public string Description { get; set; }
+        public int YearOfRelease { get; set; }
+    }
+}
